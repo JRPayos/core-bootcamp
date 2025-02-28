0e21375cec07385ee351ccc29517f53724fdc50e
JRPayos <john.rey.payos@adamson.edu.ph>
2025-02-28 13:32:17 +0800

    modified:   05-submit-tx/03-submit.ts 	modified:   README.md

--- 05-submit-tx/03-submit.ts
+++ 05-submit-tx/03-submit.ts
@@ -1,7 +1,7 @@
 // 0x3b26E8DA9aDedAAe86a260b6354aC1855AA65C14
 
 import path from 'path'
-require('dotenv').config({ path: path.resolve(__dirname, '.env') });
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
 import {
   Hex,
@@ -33,10 +33,10 @@ const account = privateKeyToAccount(`0x${privateKey}` as Hex);
   });
 
   const tx = await contract.write.recordSubmission([
-    'markneri',
-    'kimerran',
-    '0x3b26E8DA9aDedAAe86a260b6354aC1855AA65C14',
-    '0x3b26E8DA9aDedAAe86a260b6354aC1855AA65C14'
+    'John Rey',
+    'JRey',
+    '0x199499D420Ba9d5631eB8E6f23935b201fdd619A',
+    '0xB1B63BE668Eb8f8612AE8a28AE399227b75d7Bb2'
     ]);    
   console.log('tx', tx);
 })();
